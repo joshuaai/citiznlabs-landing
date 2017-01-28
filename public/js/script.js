f5e168211fac9aa248bde5b967f776fd11ae5aa6
josoftme <joshuaiheji@gmail.com>
2017-01-28 22:27:11 +0100

    Add Smooth Scrolling and Fiz Navbar to Top
    This commit allows the navbar to be fixed while scrolling using the
    navbar-fixed-top bootstrap class and setting the navber css position to
    fixed.
    
    The javascript file is updated to include logic for smooth scrolling.

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,36 @@
 $(function () {
    "use strict";
 
-   //get the height of the window
-   var windowHeight = $(window).height();
+    //get the height of the window
+    var windowHeight = $(window).height();
 
-   //variable for menu height
-   var menuHeight = 80; 
+    //variable for menu height
+    var menuHeight = 80; 
 
-   //set to window tallness
-   $('.fullheight').css('height', windowHeight); 
+    //set to window tallness
+    $('.fullheight').css('height', windowHeight); 
 
-   //replace IMG inside carousel with a background image 
+    //replace IMG inside carousel with a background image 
     $('#featured .item img').each(function() {
         var imgSrc = $(this).attr('src');
         $(this).parent().css({'background-image': 'url('+imgSrc+')'});
         $(this).remove();
     });
+
+    //Use smooth scrolling when clicking on navigation
+    $('.navbar a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') === 
+            this.pathname.replace(/^\//,'') && 
+            location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top-menuHeight+2
+                }, 500);
+                return false;
+            } //target.length
+        } //click function
+    }); //smooth scrolling
 
 });
